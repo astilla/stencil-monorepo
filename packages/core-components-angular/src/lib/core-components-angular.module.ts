29afcc91a934f31a8f5b42f01dc0486c88922b4f
Andrew Bate <andrew.bate@technologyfromsage.com>
2023-11-06 12:54:36 +0000

    Initial commit

---
+++ packages/core-components-angular/src/lib/core-components-angular.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DIRECTIVES } from '../generated/directives';
+
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [...DIRECTIVES],
+  exports: [...DIRECTIVES]
+ })
+ export class CoreComponentsAngularModule {}
+ 
